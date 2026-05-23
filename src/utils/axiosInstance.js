09f37233459ed8ef19aa239751d302b66eeaa94e
Tamilarasan-devs <tamilarasan.softwarengineer@gmail.com>
2026-05-23 13:19:01 +0530

    api

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import { tokenStore, AUTH_LOGOUT_EVENT } from './tokenStore'
 
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://aayubakwath-backend-production-2e7f.up.railway.app'
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://aayubakwath-backend-production-2e7f.up.railway.app'
+
+// updateign
+// const BASE_URL = "http://localhost:5001"
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api/v1`,
